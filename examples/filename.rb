04b2113e0ee718fb3408a70b2908f273bbf86def
David Dollar <ddollar@gmail.com>
2008-06-12 03:13:11 -0400

    remove deprecated examples

--- examples/filename.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'helper.rb'
-
-preferences = Preferences::Manager.new :preferences_test, true
-puts "GLOBAL : #{preferences.filename}"
-
-preferences = Preferences::Manager.new :preferences_test, false
-puts "USER   : #{preferences.filename}"
