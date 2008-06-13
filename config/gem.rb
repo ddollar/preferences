1014d0c749c00e6f4a1ebc1095c8a9963688c382
David Dollar <ddollar@gmail.com>
2008-06-13 13:37:11 -0400

    de-generic'd configs

--- config/gem.rb
+++ config/gem.rb
@@ -1,9 +1,9 @@
-@config.name     = "generic"
+@config.name = "preferences"
 
 require "lib/#{@config.name}"
 
-@config.version  = Generic.version
-@config.summary  = "Summary Here"
+@config.version  = Preferences.version
+@config.summary  = "An easy, cross-platform way to manage application configuration data"
 @config.homepage = "http://peervoice.com/software/#{@config.name}"
 
 @config.author_name  = "David Dollar"
